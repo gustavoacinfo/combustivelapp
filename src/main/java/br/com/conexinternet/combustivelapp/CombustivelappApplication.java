4c1fcdb6d3305c7a05b8ddcaf32433a94c989e13
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-05-02 20:58:42 -0300

    Projeto criado

---
+++ src/main/java/br/com/conexinternet/combustivelapp/CombustivelappApplication.java
@@ -0,0 +1,13 @@
+package br.com.conexinternet.combustivelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombustivelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombustivelappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/conexinternet/combustivelapp/CombustivelappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conexinternet.combustivelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombustivelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
