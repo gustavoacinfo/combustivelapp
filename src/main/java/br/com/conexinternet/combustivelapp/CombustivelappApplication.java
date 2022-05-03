26f1f221b650473882a5d55054b5e9212bd1356e
Gustavo Alves <gustavo.acinfo@gmail.com>
2022-05-03 20:26:32 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/conexinternet/combustivelapp/CombustivelappApplication.java
+++ src/main/java/br/com/conexinternet/combustivelapp/CombustivelappApplication.java
@@ -1,13 +1,33 @@
 package br.com.conexinternet.combustivelapp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CombustivelappApplication {
+import br.com.conexinternet.combustivelapp.domain.Regiao;
+import br.com.conexinternet.combustivelapp.repositories.RegiaoRepository;
 
+@SpringBootApplication
+public class CombustivelappApplication implements CommandLineRunner {
+	
+	@Autowired
+	private RegiaoRepository regiaoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CombustivelappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Regiao reg1 = new Regiao(null, "UNAI", 5.00);
+		Regiao reg2 = new Regiao(null, "INTERIOR", 4.30);
+		
+		regiaoRepository.saveAll(Arrays.asList(reg1, reg2)); 
+		
 	}
 
 }
